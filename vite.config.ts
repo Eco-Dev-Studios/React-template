0a15f3c99a96861c28f8c6295393fae8eeb2a7b1
cesswhite <acessloop@gmail.com>
2021-12-09 20:20:57 -0600

    Add auto import, icons universal and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import AutoImport from "unplugin-auto-import/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+      ],
+      imports: ["react"],
+      resolvers: [
+        IconsResolver({
+          extension: "jsx",
+        }),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+      scale: 1.2,
+      compiler: "jsx",
+      jsx: "react",
+    }),
+  ],
+});
